07a874dc8686922c624c71091f4d3bb4161e8c2b
qwhiting <quinnwhiting@yahoo.com>
2025-11-20 13:14:40 -0600

    changes iscFlag calculation if_else statement to reduce errors

--- R/iscFlag.R
+++ R/iscFlag.R
@@ -7,10 +7,13 @@
 
 iscFlag<-function(df){
   df.isc<-df%>%filter(resultQcIdentifier=="LVM")%>%filter(analyteType=="TRG")
-  df.isc$iscFlag<-if_else(df.isc$recovery<70 | df.isc$recovery>130, "LVM", NA)
+  #df.isc$iscFlag<-if_else(df.isc$recovery<70 | df.isc$recovery>130, "LVM", NA)
+  df.isc$iscFlag<-if_else(df.isc$recovery<70, "LVM", NA)
+  df.isc$iscFlag<-if_else(df.isc$recovery>130, "LVM", df.isc$iscFlag)
+
   #remove flag if isc is below ICAL LLOD
   df.isc$iscFlag<-if_else(str_detect(df.isc$limFlag, "LTL"), NA, df.isc$iscFlag)
-  #match to all other relevant samples (exclude CAL, CLC)
+  #match to all other relevant samples (exclude CAL, CLC) the ISC corresponds to entire batch
   df$iscFlag<-df.isc$iscFlag[match(df$Component.Name, df.isc$Component.Name)]
   df$iscFlag<-if_else(df$resultQcIdentifier %in% c("CAL","CLC"), NA, df$iscFlag)
   #remove flag if <MDL, <LTL, or UND
